1194c24f6670730c5ab2c6bb0d9cd26fd26cbbf6
carlitossaul <carlitossaul.h@gmail.com>
2023-01-25 00:11:22 -0300

    s

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,13 +2,20 @@ import style from "./Form.module.css"
 import { useState } from "react";
 
 import React from 'react';
+import validation from "./validation";
 
 const Form = () => {
     const [userData, setUserData] = useState({ username: '', password: '' });
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState({ username: '', password: '' })
 
     const handleChange = (event) => {
         setUserData({ ...userData, [event.target.name]: event.target.value })
+
+        setErrors(validation({
+            ...userData,
+            [event.target.name]: event.target.value
+        }))
+
     }
 
     return (
@@ -17,10 +24,12 @@ const Form = () => {
                 <div className={style.username}>
                     <label className={style.label} htmlFor="username">Username</label>
                     <input onChange={handleChange} type="text" name="username" placeholder="Write your username..." value={userData.username} className={style.input} />
+                    <span>{errors.username ? errors.username : ""}</span>
                 </div>
                 <div className={style.password}>
                     <label className={style.label} htmlFor="password">Password</label>
                     <input onChange={handleChange} type="password" name="password" placeholder="Write your password..." value={userData.password} className={style.input} />
+                    <span>{errors.password ? errors.password : ""}</span>
                 </div>
                 <div className={style.button}>
                     <button className={style.login} >LOGIN</button>
--- src/components/Form/validation.js
+++ src/components/Form/validation.js
@@ -1,12 +1,19 @@
 
 const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$/;
 
-const validation = (userData) => {
+const validation = ({ username, password }) => {
     const errors = {}
 
-    if (username !== regexEmail.test(userData)) errors.username = "the user must be an email address"
-    if (!username) errors.username = "the user name cannot be empty"
-    if (username > 35) errors.username = "the user name must not be longer than 35 characters"
+    if (!regexEmail.test(username)) errors.username = "El nombre de ususario debe ser un email"
+    if (!username) errors.username = "el nombre de usuario no debe estar vacio"
+    if (username.length > 35) errors.username = "el nombre de usuario no debe tener mas de 35 caracteres"
 
+    if (!regexPass.test(password)) errors.password = "la contraseña debe tener al menos un dígito numérico , una letra mayúscula y una letra minúscula"
+    if (password.length < 6 || password.length > 10) errors.password = "la contraseña debe tener entre 6 y 10 caracteres"
+
+    return errors
 
 }
+
+export default validation
