26a5d37770510384ef593472e6b89508ac32e16a
Carlitossaul <carlitossaul.h@gmail.com>
2023-03-18 18:20:34 -0300

    finish

--- back/src/controllers/index.js
+++ back/src/controllers/index.js
@@ -1,25 +1,26 @@
 const axios = require("axios"); //esta forma de importar commonjs
 var favs = require("../utils/favs");
 
-async function getCharacterId(req, res) {
+function getCharacterId(req, res) {
   const { id } = req.params;
-  const response = await axios.get(
-    `https://rickandmortyapi.com/api/character/${id}`
-  );
-
-  try {
-    let character = {
-      id: response.data.id,
-      name: response.data.name,
-      image: response.data.image,
-      gender: response.data.gender,
-      species: response.data.species,
-    };
-    res.status(200).json(character);
-  } catch (error) {
-    res.status(500).send(error.message);
-    console.log(error.message);
-  }
+  console.log(id);
+  axios
+    .get(`https://rickandmortyapi.com/api/character/${id}`) //llamado de un personaje en especifico
+    .then(
+      ({ data }) => {
+        let character = {
+          id: data.id,
+          name: data.name,
+          image: data.image,
+          gender: data.gender,
+          species: data.species,
+        };
+        res.status(200).json(character); //cuando envio la informacion la parseo
+      },
+      (err) => {
+        res.status(500).send(err.message);
+      }
+    );
 }
 
 async function getDetailId(req, res) {
@@ -53,20 +54,20 @@ module.exports = {
   getDetailId,
 };
 
-// axios
-//   .get(`https://rickandmortyapi.com/api/character/${id}`) //llamado de un personaje en especifico
-//   .then(
-//     ({ data }) => {
-//       let character = {
-//         id: data.id,
-//         name: data.name,
-//         image: data.image,
-//         gender: data.gender,
-//         species: data.species,
-//       };
-//       res.status(200).json(character); //cuando envio la informacion la parseo
-//     },
-//     (err) => {
-//       res.status(500).send(err.message);
-//     }
-//   );
+// const response = await axios.get(
+//   `https://rickandmortyapi.com/api/character/${id}`
+// );
+
+// try {
+//   let character = {
+//     id: response.data.id,
+//     name: response.data.name,
+//     image: response.data.image,
+//     gender: response.data.gender,
+//     species: response.data.species,
+//   };
+//   res.status(200).json(character);
+// } catch (error) {
+//   res.status(500).send(error.message);
+//   console.log(error.message);
+// }
