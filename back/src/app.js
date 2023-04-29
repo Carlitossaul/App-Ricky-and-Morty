76e7d06ecc827cbd03128a88f806dee2af932180
Carlitossaul <carlitossaul.h@gmail.com>
2023-04-29 16:59:44 -0300

    Para deployar

--- back/src/app.js
+++ back/src/app.js
@@ -10,12 +10,27 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
 // Configuración con problema de CORS
-const corsOptions = {
-  origin: "https://multiverse-rym.vercel.app",
-  // origin: "*",
-  credentials: true, // access-control-allow-credentials: true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "https://multiverse-rym.vercel.app",
+//   // origin: "*",
+//   credentials: true, // access-control-allow-credentials: true
+//   optionSuccessStatus: 200,
+// };
+
+server.use((req, res, next) => {
+  //https://youarehungry.vercel.app
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://multiverse-rym.vercel.app"
+  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(cors(corsOptions));
 app.use(express.json()); //para que funcione mi servidor con formato json
