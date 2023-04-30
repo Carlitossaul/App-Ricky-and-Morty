4a91d08824c2e18f21cf4fc81644d433d82e5d53
Carlitossaul <carlitossaul.h@gmail.com>
2023-04-30 11:42:16 -0300

    s

--- back/src/app.js
+++ back/src/app.js
@@ -18,10 +18,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 // };
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://rym-multiverse.vercel.app"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // "https://rym-multiverse.vercel.app"
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
