f37b49b831c363f9c1ce0376504822eebc4f9812
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-01 18:37:18 -0300

    s

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import image from "../../assets/photo1674935309.jpeg";
 import style from "./About.module.css";
+import { FaLinkedin } from "react-icons/fa";
+import { GoMarkGithub } from "react-icons/go";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
 const About = () => {
   return (
@@ -25,7 +28,38 @@ const About = () => {
           <p className={style.parrafo}>I am a full stack web developer</p>
           <br />
           <h1 className={style.h1}>Contact:</h1>
-          <p className={style.parrafo}>In process</p>
+          <div className={style.links}>
+            <a
+              target="_blank"
+              className={style.link}
+              href="https://www.linkedin.com/in/carloslovey"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className={style.icon} />
+              LinkedIn:My professional profile on LinkedIn, where you can find
+              information about my work experience and skills. Click here!
+            </a>
+            <a
+              target="_blank"
+              className={style.link}
+              href="https://github.com/Carlitossaul"
+              rel="noopener noreferrer"
+            >
+              <GoMarkGithub className={style.icon} />
+              GitHub: "My GitHub profile, where I share some of my projects and
+              collaborations with the developer community. Click here!
+            </a>
+            <a
+              target="_blank"
+              className={style.link}
+              href="https://carloslovey.vercel.app/"
+              rel="noopener noreferrer"
+            >
+              <BsBoxArrowUpRight className={style.icon} />
+              Portfolio: My portfolio of projects, where you can see some of the
+              work I have done and my work style. Click here!
+            </a>
+          </div>
         </div>
         <div className={style.divImage}>
           <img className={style.image} src={image} alt="profile" />
