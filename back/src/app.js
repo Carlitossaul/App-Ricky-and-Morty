017b871867b6161da603ca57b595423a8263c336
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-22 15:10:54 -0300

    actualizacion de la app

--- back/src/app.js
+++ back/src/app.js
@@ -10,10 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://rym-multiverse.vercel.app"
-  ); // http://localhost:3000
+  res.header("Access-Control-Allow-Origin", "*"); // http://localhost:3000 https://rym-multiverse.vercel.app
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
