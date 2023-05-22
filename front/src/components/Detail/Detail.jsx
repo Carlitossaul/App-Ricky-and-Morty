2d12bf58c78639d344b372ad760c9bdb0177c60c
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-22 15:41:58 -0300

    actualizacion de la app

--- front/src/components/Detail/Detail.jsx
+++ front/src/components/Detail/Detail.jsx
@@ -11,10 +11,10 @@ const Detail = () => {
 
   useEffect(() => {
     axios(`/rickandmorty/detail/${detailId}`)
-      .then((char) => {
+      .then(({ data }) => {
         console.log(char);
         if (char.name) {
-          setCharacter(char);
+          setCharacter(data);
         }
       })
       .catch((err) => {
